7682b6f00d778c489552f4b334dab7d0fa7f7677
minwoogi1007 <minwoogi@twowincall.com>
2023-07-28 20:28:56 +0900

    Initial commit

---
+++ src/test/java/com/twocrm/TwoCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twocrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwoCrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
